fa2a615d42f9b29544e6cc26039b7d1000ad0f72
hiroshimashu <hiroshimashu@gmail.com>
2020-11-13 15:06:05 +0900

    add: basic render function

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
-function hello(name: string): string {
-	return `Hello, ${name}!`;
-}
+import Color from './Color';
 
-console.log(hello('World'));
+function Render(ctxWidth: number, ctxHeight: number): number[][] {
+	const colorPixelArray = [];
+	for (let i = 0; i < ctxWidth - 1; i++) {
+		for (let j = 0; j < ctxHeight - 1; j++) {
+			const r = Math.random();
+			const g = Math.random();
+			const b = Math.random();
+			const color = new Color(r, g, b);
+
+			colorPixelArray.push([...color.toRGBArray(), 1]);
+		}
+	}
+	return colorPixelArray;
+}
