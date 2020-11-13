3d67f2d8730b90da0fbab2ed3973d943fe8ad1cc
hiroshimashu <hiroshimashu@gmail.com>
2020-11-13 14:29:01 +0900

    add: test for Color

---
+++ src/__tests__/Color.test.ts
@@ -0,0 +1,15 @@
+import Color from '../Color';
+
+test('Intialize color', () => {
+	const color = new Color(0.2, 0.8, 0.6);
+
+	expect(color.r).toBe(0.2);
+	expect(color.g).toBe(0.8);
+	expect(color.b).toBe(0.6);
+});
+
+test('Convert color to rgb string', () => {
+	const color = new Color(0.2, 0.8, 0.6);
+
+	expect(color.toString()).toBe(`${114} ${228} ${198}`);
+});
